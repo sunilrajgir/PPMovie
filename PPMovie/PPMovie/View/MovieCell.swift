80340fd77dce4841b7fa7987993c875216aced4f
Sunil Kumar <sunil.kumar@telekom-digital.com>
2021-12-18 17:47:59 +0530

    linked cell for tableview copntroller

---
+++ PPMovie/PPMovie/View/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  PPMovie
+//
+//  Created by Sunil Kumar on 18/12/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    static let identifier = "MovieCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- PPMovie/PPMovie/View/MovieViewController.swift
+++ PPMovie/PPMovie/View/MovieViewController.swift
@@ -8,25 +8,53 @@
 import UIKit
 
 class MovieViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
     var viewModel : MovieViewModel!
     convenience init(viewModel: MovieViewModel) {
         self.init(nibName: nil, bundle: nil)
         self.viewModel = viewModel
     }
-
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
         viewModel.getData {
             
         }
+    }
+    
+    private func setupTableView() {
+        tableView.register(UINib(nibName: MovieCell.identifier, bundle: nil), forCellReuseIdentifier: MovieCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200.0
     }
 }
